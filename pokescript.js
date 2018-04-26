8ea6f7145bfa21bc9746ffe333a17533e88dc41a
Alexander George Legaspi <lex83nyc@gmail.com>
2018-04-25 20:29:41 -0400

    second commit

--- pokescript.js
+++ pokescript.js
@@ -1,24 +1,5 @@
-var ajaxhttp = new XMLHttpRequest();
-var url = "jynx.json";
 
-ajaxhttp.open("GET", url, true);
-ajaxhttp.setRequestHeader("content-type", "application/json");
-ajaxhttp.onreadystatechange = function () {
-  if (ajaxhttp.readyState == 4 && ajaxhttp.status == 200) {
-
-    var jcontent = JSON.parse(ajaxhttp.responseText);
-      console.log(jcontent);
-      console.log(ajaxhttp);
-  }
-}
-
-ajaxhttp.send(null);
-console.log(ajaxhttp);
-
-
-
-
-// axios.get('https://bit.ly/2HU6L4F')
-// .then(function (response) {
-//   console.log(response)
-// });
+axios.get('https://github.com/aglegaspi/pokedex/blob/master/jynx.json')
+.then(function (response) {
+  console.log(response)
+});
